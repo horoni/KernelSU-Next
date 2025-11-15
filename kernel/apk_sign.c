92e1d77771675492197e720f36d3aa23b54b725a
horoni <71940406+horoni@users.noreply.github.com>
2025-11-15 08:49:31 +0000

    Fix kernel_compat.c amd apk_sign.c

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -340,9 +340,9 @@ bool ksu_is_manager_apk(char *path)
 		path, expected_manager_size, expected_manager_hash);
 
 #ifdef CONFIG_KSU_SUSFS
-	return (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH) ||
+	return (check_v2_signature(path, expected_manager_size, expected_manager_hash) ||
 			check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4") || // 5ec1cff
-      check_v2_signature(path, 384, "b4808bbe6bb6844c093dc2baaeb5b9c6ed1ad4f0c7b23dd265290c080ccd6a1e")); // horoni 
+      check_v2_signature(path, 0x300, "b4808bbe6bb6844c093dc2baaeb5b9c6ed1ad4f0c7b23dd265290c080ccd6a1e")); // horoni 
 #else
 	return check_v2_signature(path, expected_manager_size, expected_manager_hash);
 #endif
